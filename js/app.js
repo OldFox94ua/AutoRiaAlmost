e3b6e5222ba2213b4b6abf7a038e815b397c3185
Vadim <mitai3627@gmail.com>
2021-02-17 00:01:58 +0200

    feat: added sorting functionality
    the functionality of sorting of the goods is made

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 let CARS = JSON.parse(DATA)
 const cardListEl = document.getElementById('cardList')
-
+const sortSelectEl = document.getElementById('sortSelect')
+const masonryBtnsEl = document.getElementById('masonryBtns')
 
 
 // {
@@ -26,16 +27,56 @@ const cardListEl = document.getElementById('cardList')
 //     "odo": 394036,
 //     "consume": { "road": 4.8, "city": 12.3, "mixed": 8.4 }
 //   }
+masonryBtnsEl.addEventListener('click', event => {
+  const btnEl = event.target.closest('.btn')
+  if (btnEl) {
+    const view = btnEl.dataset.view
+    if (view == '1') {
+      cardListEl.classList.remove('row-cols-2')
+      cardListEl.classList.add('row-cols-1')
+    } else if (view == '2'){
+      cardListEl.classList.remove('row-cols-1')
+      cardListEl.classList.add('row-cols-2')
+    }
+    btnEl.classList.remove('btn-secondary')
+    btnEl.classList.add('btn-success')
+    findSiblings(btnEl).forEach(sibling => {
+      sibling.classList.remove('btn-success')
+      sibling.classList.add('btn-secondary')
+    })
+  }
+})
 
-
+sortSelectEl.addEventListener('change', function (event) {
+  let [key, type] = this.value.split('-')
+  if (type == 'ab') {
+    CARS.sort((a,b) => {
+      if (typeof a[key] === 'string') {
+        return a[key].localeCompare(b[key])
+      } else if (typeof a[key] === 'number' || typeof a[key] === 'boolean') {
+        return a[key] - b[key]
+      }
+    })
+  } else if(type == 'ba'){
+    CARS.sort((a,b) => {
+      if (typeof b[key] === 'string') {
+        return b[key].localeCompare(a[key])
+      } else if (typeof b[key] === 'number' || typeof b[key] === 'boolean') {
+        return b[key] - a[key]
+      }
+    })
+  }
+  renderCards(CARS, cardListEl)
+})
 
 renderCards(CARS, cardListEl)
 
 function renderCards(dataArray, DOMelement) {
+    let html = ''
     dataArray.forEach(car => {
-        let html = createCard(car)
-        DOMelement.insertAdjacentHTML('beforeEnd', html)
+        html += createCard(car)
     });
+    DOMelement.innerHTML = html
 }
 
 function createCard(data) {
@@ -50,21 +91,21 @@ function createCard(data) {
 
     return `<div class="col card mb-3">
     <div class="row g-0">
-      <div class="col-4 position-relative">
+      <div class="col-4 card-img-wrap position-relative">
       ${data.top ? `<div class="card-top bg-success text-light">TOP</div>` : ''}
         <img src="${data.img}" alt="${data.make} ${data.model}" class="card-img" width="1" height="1" loading="lazy" />
       </div>
-      <div class="col-8">
+      <div class="col-8 card-body-wrap">
         <div class="card-body">
           <h3 class="card-title">${data.make} ${data.model} ${data.engine_volume} (${data.year})</h3>
           <h4 class="card-price">${data.price}$</h4>
           <ul class="info-block">
-            <li><i class="fas fa-tachometer-alt"></i>${data.odo}km</li>
+            <li><i class="fas fa-tachometer-alt"></i>${data.odo} km</li>
             <li><i class="fas fa-map-marker-alt"></i>${data.country}</li>
-            <li><i class="fas fa-gas-pump me-10"></i>${data.fuel} ${data.engine_volume}L</li>
+            <li><i class="fas fa-gas-pump me-10"></i>${data.fuel} ${data.engine_volume} L</li>
             <li><i class="fas fa-cogs"></i>${data.transmission}</li>
           </ul>
-          <h5 class="card-rating text-warning"> ${starIcons} ${data.rating}</h5>
+          <h5 class="card-rating text-warning"> ${starIcons}</h5>
           ${data.vin ? `<p class="card-vin ${data.vin_check ? 'text-success' : 'text-warning'}">${data.vin_check ? `<i class="fas fa-check me-2"></i>` : `<i class="fas fa-times me-2"></i>`}${data.vin}</p>` : `<p class="card-vin">Продавець не надав VIN код!</p>`}
           <a href="tel:${data.phone}" class="btn btn-success call-btn">Call</a>
         </div>
@@ -77,5 +118,15 @@ function createCard(data) {
       </div>
     </div>
   </div>`
+}
+
+
+//Utils
+function findSiblings(DOMelement) {
+  // const parent = DOMelement.parentElement
+  // const children = parent.children
+  // const siblings = Array.from(children).filter(child => child != DOMelement)
+  // return siblings
+  return Array.from(DOMelement.parentElement.children).filter(child => child != DOMelement)
 }
 
